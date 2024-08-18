240961157ddd526f40ff06036f61ee58e8dbd457
Will <author@will.party>
2024-08-17 17:31:03 -0700

    Update generation script to use a nice color by default

--- generate-color-theme.sh
+++ generate-color-theme.sh
@@ -8,12 +8,12 @@ UNDERLINE='\033[3;4m'
 RESET='\033[0m'
 
 printf $BLUE
-printf '___________.__          __    __________               .__         \n'; sleep 0.1
-printf '\_   _____/|  | _____ _/  |_  \______   \ ____   _____ |__|__  ___ \n'; sleep 0.1
-printf ' |    __)  |  | \__  \\   __\  |       _// __ \ /     \|  \  \/  / \n'; sleep 0.1
-printf ' |     \   |  |__/ __ \|  |    |    |   \  ___/|  Y Y  \  |>    <  \n'; sleep 0.1
-printf ' \___  /   |____(____  /__|    |____|_  /\___  >__|_|  /__/__/\_ \ \n'; sleep 0.1
-printf '     \/              \/               \/     \/      \/         \/ \n'; sleep 0.1
+printf '___________.__          __    __________               .__         \n'
+printf '\_   _____/|  | _____ _/  |_  \______   \ ____   _____ |__|__  ___ \n'
+printf ' |    __)  |  | \__  \\   __\  |       _// __ \ /     \|  \  \/  / \n'
+printf ' |     \   |  |__/ __ \|  |    |    |   \  ___/|  Y Y  \  |>    <  \n'
+printf ' \___  /   |____(____  /__|    |____|_  /\___  >__|_|  /__/__/\_ \ \n'
+printf '     \/              \/               \/     \/      \/         \/ \n'
 printf $RESET
 
 if [ $# -lt 3 ]
@@ -21,7 +21,6 @@ then
   printf "    ${GREEN}Usage:${RESET} ${UNDERLINE}$0${RESET} ${ORANGE}<VARIANT_NAME> <HIGHLIGHT_HEX_COLOR> <HIGHLIGHT_TEXT_HEX_COLOR>${RESET}\n"
   printf "                                                                                                                                  \n"
   printf "        ${GREEN}Example:${RESET} ${UNDERLINE}$0${RESET} ${ORANGE}FOOBAR '#123456' '#987654'${RESET}                               \n"
-  EXIT=1
 fi
 
 command -v make > /dev/null \
@@ -37,11 +36,9 @@ command -v optipng > /dev/null \
 [ -z $EXIT ] || exit 1
 
 
-sleep 1
-
-eval export COLOR_VARIANTS=$1
-eval export COLOR_$COLOR_VARIANTS=$2
-eval export TEXT_COLOR_$COLOR_VARIANTS=$3
+eval export COLOR_VARIANTS="${1:-Nice}"
+eval export COLOR_$COLOR_VARIANTS="${2:-#2c3300}"
+eval export TEXT_COLOR_$COLOR_VARIANTS="${3:-#f2f2f2}"
 
 rm -rf themes/*
 make -j build
